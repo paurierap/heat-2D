2e97f2db2b38a657e6252fbce87d69f41fc25093
Pau Riera <pau.riera.p@gmail.com>
2026-04-04 00:50:45 +0100

    Create class SolutionWriter

---
+++ heat2D/SolutionWriter/SolutionWriter.hpp
@@ -0,0 +1,45 @@
+#ifndef SOLUTIONWRITER_HPP
+#define SOLUTIONWRITER_HPP
+
+#include <Eigen/Dense>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "Mesh2D.hpp"
+
+class SolutionWriter
+{
+    private:
+        std::ofstream filename_;
+
+    public: 
+        SolutionWriter(const std::string& filename) : filename_(filename)
+        {
+            if (!filename_.is_open()) throw std::runtime_error("Could not open file to write solution\n");
+
+            // Write header for output file
+            filename_ << "x,y,u,t\n";
+        }
+
+        ~SolutionWriter() = default;
+
+        // Make uncopyable
+        SolutionWriter(const SolutionWriter&) = delete;
+        SolutionWriter& operator=(const SolutionWriter&) = delete;
+
+        void write(const spatial::Mesh2D& mesh, const Eigen::VectorXd& solution,  double t)
+        {
+            std::vector<spatial::Node2D> nodes = mesh.getNodes();
+            
+            int i = 0;
+            for(const auto& node : nodes)
+            {
+                filename_ << node.x_ << "," << node.y_ << "," << solution[i] << "," << t << "\n";
+                ++i;
+            }
+        }
+};
+
+#endif
