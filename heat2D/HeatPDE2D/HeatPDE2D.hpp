cab24cdafe29a51c16f7eaa902d629239729000f
Pau Riera <pau.riera.p@gmail.com>
2026-04-06 18:37:52 +0100

    Replate t_start by t_current for a simpler getSolution

--- heat2D/HeatPDE2D/HeatPDE2D.hpp
+++ heat2D/HeatPDE2D/HeatPDE2D.hpp
@@ -14,29 +14,29 @@ class HeatPDE2D
         spatial::SpatialDiscretization2D& spatial_discretization_;
         temporal::TimeIntegrator& time_integrator_;
         
-        // Initial conditions
-        double t_start_;
+        // Initial condition
         std::function<double (double, double)> u_start_;
         
-        Eigen::VectorXd u_end_;
+        double t_current_;
+        Eigen::VectorXd u_current_;
 
     public:
         HeatPDE2D(spatial::SpatialDiscretization2D& spatial_discretization, temporal::TimeIntegrator& time_integrator, double t_start, std::function<double (double, double)> u_start) 
         : spatial_discretization_(spatial_discretization),
         time_integrator_(time_integrator),
-        t_start_(t_start),
-        u_start_(u_start)
+        u_start_(u_start),
+        t_current_(t_start)
         {
             spatial_discretization_.discretize();
 
-            u_end_ = spatial_discretization_.reduce(u_start);
+            u_current_ = spatial_discretization_.reduce(u_start);
             
             // Cache necessary matrices (depending on the time integration scheme)
             time_integrator_.setUp(spatial_discretization_);
         };
 
         // Getter
-        Eigen::VectorXd getSolution() const {return spatial_discretization_.fillDirichletNodes(u_end_);};
+        Eigen::VectorXd getSolution() const {return spatial_discretization_.fillDirichletNodes(u_current_, t_current_);};
 
         // DefaultCallback generates a default dummy function that does nothing when no callback function is provided.
         struct DefaultCallback 
@@ -48,33 +48,35 @@ class HeatPDE2D
         template <typename CallbackFunction = DefaultCallback>
         void integrate(double t_end, CallbackFunction&& callback = {})
         {
-            if (t_end <= t_start_) throw std::invalid_argument("T_end must be larger than t_start.");
+            if (t_end <= t_current_) throw std::invalid_argument("t_end must be larger than current time.");
 
             const double dt = time_integrator_.getTimestep();
-            const int n_steps = static_cast<int>(std::floor((t_end - t_start_) / dt));
+            const int n_steps = static_cast<int>(std::floor((t_end - t_current_) / dt));
             int step_count = 0;
 
-            while (step_count < n_steps) 
+            for (int step_count = 0; step_count < n_steps; ++step_count)
             {
-                time_integrator_.step(spatial_discretization_, t_start_ + step_count * dt, u_end_);
-                step_count++;
-
+                time_integrator_.step(spatial_discretization_, t_current_, u_current_);
+                t_current_ += dt;
+            
                 // For IO of the solution
-                callback(t_start_ + step_count * dt, u_end_);
+                callback(t_current_, getSolution());
             }
-
-            const double remainder = t_end - (t_start_ + dt * n_steps);
+            
+            const double remainder = t_end - t_current_;
             if (remainder > 1e-10 * dt)
             {
                 std::unique_ptr<temporal::TimeIntegrator> tail = time_integrator_.cloneWithTimestep(remainder);
                 tail->setUp(spatial_discretization_);
-                tail->step(spatial_discretization_, t_start_ + dt * n_steps, u_end_);
+                tail->step(spatial_discretization_, t_current_, u_current_);
+
+                t_current_ = t_end;
 
                 // For IO of the solution
-                callback(t_start_ + step_count * dt, u_end_);
+                callback(t_current_, getSolution());
             }
 
-            t_start_ = t_end;
+            t_current_ = t_end;
         }
 };
 
